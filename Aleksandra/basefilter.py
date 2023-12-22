10f1a224bf6ed998927af8a1d01cd7bc875e3755
Aleksandra Soboleva <sobolale@cvut.cz>
2023-12-22 16:43:26 +0100

    Adding a filter base frame (write result)

---
+++ Aleksandra/basefilter.py
@@ -0,0 +1,27 @@
+#not related to simple filters
+#should work, but not tested
+import os
+import random
+from corpus import Corpus 
+
+class BaseFilter:
+    def __init__(self, directory_path=None, filter_output = None, file="!truth.txt"):
+  
+        self.directory_path = directory_path
+        self.filter_output = filter_output
+        self.file = file
+    
+    def test(self):
+        file = os.path.join(self.directory_path, self.file)
+        with open(file, "wt", encoding="utf-8") as f:
+            for email_name, _ in Corpus(self.directory_path).emails():
+                if email_name != 0:
+                    f.write("\n")
+                    
+                if self.filter_output is None:
+                    self.filter_output = random.choice(["SPAM", "OK"])
+                
+                f.write(f'{email_name} {self.filter_output}')
+
+                
+    
