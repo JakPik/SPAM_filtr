59d00c76b463f58f0fa6159edf61f843dc6d70d3
Aleksandra Soboleva <sobolale@cvut.cz>
2023-12-24 21:26:28 +0100

    Renaming a variable (just to keep the code clean).

--- SPAM_folder/corpus.py
+++ SPAM_folder/corpus.py
@@ -1,8 +1,8 @@
 import os
 
 class Corpus:
-    def __init__(self, folder_path):
-        self.mail_folder_path = folder_path
+    def __init__(self, directory_path):
+        self.mail_folder_path = directory_path
 
     def emails(self):
         for filename in os.listdir(self.mail_folder_path):
