405cb594c61e1fcea8436e99a0dcb785ee787057
Aleksandra Soboleva <sobolale@cvut.cz>
2023-12-26 20:32:52 +0100

    Add filtration by email subject. But I'm not sure if this is effective, because the quality result remains the same as without it.

--- SPAM_folder/filter.py
+++ SPAM_folder/filter.py
@@ -7,6 +7,8 @@ class MyFilter(BaseFilter):
     def __init__(self):
         super().__init__()
         self.spam_senders = []
+        self.spam_subjects = []
+
 
     def train(self, directory_path):
         training_corpus = TrainingCorpus(directory_path)
@@ -14,14 +16,17 @@ class MyFilter(BaseFilter):
         for name, body in Corpus(directory_path).emails():
             if training_corpus.get_class(name) == "SPAM":
                 sender = self.find_sender(body)
+                subject = self.find_subject(body)
                 if sender is not None and sender not in self.spam_senders:
                     self.spam_senders.append(sender)
-                # self.subject.append(self.find_subject(body)) 
+                if subject is not None and subject not in self.spam_subjects:
+                    self.spam_subjects.append(subject)
+ 
                 
     def compute_result(self, body):
         count = 0
         count += self.check_for_sender(body, self.spam_senders) 
-        # count += self.check_for_subject(body, self.subject)
+        count += self.check_for_subject(body, self.spam_subjects)
         if count != 0:
             return "SPAM"
         else:
