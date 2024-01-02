1c112a395b37857dd929173e55f540fbc3a2c933
Aleksandra Soboleva <sobolale@cvut.cz>
2024-01-02 12:51:19 +0100

    Add commets

--- SPAM_folder/corpus.py
+++ SPAM_folder/corpus.py
@@ -5,11 +5,13 @@ class Corpus:
         self.mail_folder_path = directory_path
 
     def emails(self):
+        # Generator method returning pairs (filename, email body) for each email in the directory
         for filename in os.listdir(self.mail_folder_path):
+            # Skip files starting with '!'
             if filename.startswith('!'):
                 continue
             file_path = os.path.join(self.mail_folder_path, filename)
             with open(file_path, 'r', encoding="utf-8") as f:
                 body = f.read()
-
+            # Return a pair (filename, body) using a generator
             yield filename, body
