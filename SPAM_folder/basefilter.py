f7b22692710805c49a5d6bc8cc9aa17e0ba60118
Aleksandra Soboleva <sobolale@cvut.cz>
2024-01-02 12:50:43 +0100

    Add commets

--- SPAM_folder/basefilter.py
+++ SPAM_folder/basefilter.py
@@ -5,24 +5,31 @@ from trainingcorpus import TrainingCorpus
 class BaseFilter:
     
     def __init__(self):
+        # File name to save the default predictions
         self.file = "!prediction.txt" 
         
     def train(self, directory_path):
+        # Filter training method to be overridden in subclasses
         raise NotImplementedError("Implement")
     
     def test(self, directory_path):
+        # Method for testing the filter and writing the results to a file
         self.write_to_file(directory_path)
     
     def compute_result(self, body):
+        # Method to calculate the classification result, which should be overridden in subclasses
         raise NotImplementedError("Implement")
     
     def write_to_file(self, directory_path):
+        # Method for writing predictions to a file
         file = os.path.join(directory_path, self.file)
         with open(file, "wt", encoding="utf-8")as f:
+            # Iterate through the emails in the corpus and write the results to a file
             for email_name, body in Corpus(directory_path).emails():
                 f.write(f"{email_name} {self.compute_result(body)}\n")
                 
     def find_sender(self, body):
+        # Method for searching for the sender in the text of a letter
         array = body.split()
         count = 0
         lowercased_array = [word.lower() for word in array]
@@ -33,6 +40,7 @@ class BaseFilter:
         return None
     
     def find_subject(self, body):
+        # Method for searching for a subject in the text of a letter
         array = body.split()
         count = 0
 
@@ -53,10 +61,12 @@ class BaseFilter:
         return None
     
     def check_for_sender(self, body, spam_senders):
+        # Method for checking whether a sender is in the spam sender list
         sender = self.find_sender(body)
         return 1 if sender and sender in spam_senders else 0
     
     def check_for_subject(self, body, spam_subjects):
+        # Method for checking if a topic is in the list of spam topics
         subject = self.find_subject(body)
         return 1 if subject and subject in spam_subjects else 0
         
